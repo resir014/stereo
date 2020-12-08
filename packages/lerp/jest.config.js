3b484c227f2f915adad80f309f27a7b81b41b9ab
Resi Respati <resir014@gmail.com>
2020-12-08 16:57:12 +0700

    test(lerp): fixed wrong setup config

--- packages/lerp/jest.config.js
+++ packages/lerp/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/src/jest.setup.ts'],
   collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/index.{ts,tsx}', '!src/index.{ts,tsx}']
 }

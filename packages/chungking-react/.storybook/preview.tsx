815fe5e155413de836e990f5ff406a41b8cee3b1
Resi Respati <resir014@gmail.com>
2020-12-09 17:35:19 +0700

    fix(chungking-react): attempt to fix storybook

--- packages/chungking-react/.storybook/preview.tsx
+++ packages/chungking-react/.storybook/preview.tsx
@@ -1,20 +1,15 @@
 // .storybook/preview.js
 import * as React from 'react'
-import { Global } from '@emotion/react'
-import { Theme } from '../src/Theme'
-import GlobalStyles from '../src/utils/globalStyles'
+import { ChungkingProvider } from '../src'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' }
 }
 
-// .storybook/preview.js
-
 export const decorators = [
   (Story) => (
-    <Theme>
-      <Global styles={GlobalStyles} />
+    <ChungkingProvider>
       <Story />
-    </Theme>
+    </ChungkingProvider>
   )
 ]

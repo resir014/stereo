76b729deb8847758070d428c43aad8f97c6d7e7e
Resi Respati <resir014@gmail.com>
2020-12-11 21:52:10 +0700

    test(kait): fixed tests

--- packages/kait/src/hooks/useIsMounted.test.ts
+++ packages/kait/src/hooks/useIsMounted.test.ts
@@ -4,17 +4,17 @@ import useIsMounted from './useIsMounted'
 describe('useIsMounted', () => {
   test('returns true if the component is mounted', () => {
     const { result } = renderHook(() => useIsMounted())
-    expect(result.current).toBe(true)
+    expect(result.current).toStrictEqual({ current: true })
   })
 
   test('returns false if the component is unmounted', () => {
     const { result, unmount } = renderHook(() => useIsMounted())
-    expect(result.current).toBe(true)
+    expect(result.current).toStrictEqual({ current: true })
 
     act(() => {
       unmount()
     })
 
-    expect(result.current).toBe(false)
+    expect(result.current).toStrictEqual({ current: false })
   })
 })

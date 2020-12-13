6ae5078ff9dbf250dba84666e369bceee43518b1
Resi Respati <resir014@gmail.com>
2020-12-13 17:25:33 +0700

    docs(chungking-react): fixed docs addon theme

--- packages/chungking-react/.storybook/preview.tsx
+++ packages/chungking-react/.storybook/preview.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react'
+import { themes } from '@storybook/theming'
 import { ChungkingProvider } from '../src'
 
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' }
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  docs: {
+    theme: themes.dark
+  }
 }
 
 const withThemeProvider = (Story: React.FC) => {

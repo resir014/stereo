daa648728bfebfde8d8bb50c2c1e599b01f1812d
Resi Respati <resir014@gmail.com>
2020-12-26 00:17:17 +0700

    refactor: remove `@resir014/chungking-react`
    `@resir014/chungking-react` has been moved here:
    https://github.com/resir014/chungking

--- packages/chungking-react/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './components'
-export * from './foundations'
-export * from './layout'
-export * from './utils'
-export { default as ChungkingProvider } from './ChungkingProvider'
--- packages/chungking-react/src/layout/aspect-ratio/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as AspectRatio } from './AspectRatio'
-export * from './AspectRatio'
